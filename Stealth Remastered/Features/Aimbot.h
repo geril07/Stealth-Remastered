4b5c1ff12ee7ad6e46cd1a72559dc6abbd33264c
geril <gergert.ilya@mail.ru>
2026-01-16 13:36:05 +0300

    fix Aimbot.h

--- Stealth Remastered/Features/Aimbot.h
+++ Stealth Remastered/Features/Aimbot.h
@@ -7,8 +7,11 @@ public:
 	{
 		iTargetBone = 1;
 		iTargetPlayer = -1;
+    iSilentTargetBone = 1;
+    iSilentTargetPlayer = -1;
 		bCrosshair = false;
 		vecTargetBone = { 0.f, 0.f, 0.f };
+    vecSilentTargetBone = { 0.f, 0.f, 0.f };
 		vecCrosshair = { 0.f, 0.f, 0.f };
 
 		oFireInstantHit = (tFireInstantHit)(pSAMP->g_dwSAMP_Addr + 0xB05A0);
